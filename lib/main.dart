cc04bb4f4634124c77b3fe84ccdb8bef0cfc92ae
Damla Cim <cim.damla@gmail.com>
2021-09-20 15:28:25 +0300

    rezillik gerçekten

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,46 @@ void main() {
   );
 }
 
-class MusicPage extends StatelessWidget {
+class MusicPage extends StatefulWidget {
   const MusicPage({Key? key}) : super(key: key);
 
   @override
+  _MusicPageState createState() => _MusicPageState();
+}
+
+class _MusicPageState extends State<MusicPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.black,
+            Colors.black38,
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(70.0),
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(height: 40.0),
+              Text('Çalmaya doyamadıklarım'),
+              SizedBox(height: 45.0),
+              Image.asset(
+                'images/safiyesoyman.png',
+              ),
+              SizedBox(height: 15.0),
+              Text('djfvıjvıervj')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
